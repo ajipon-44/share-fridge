2a9b9006613e9397a90c7036ea8e68b7f4c9776c
ajipon-44 <63995620+ajipon-44@users.noreply.github.com>
2025-09-08 00:16:26 +0900

    feat: create docker file for pd

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  const res = await fetch("http://localhost:8080");
+  const res = await fetch(`${process.env.API_URL}`, { cache: "no-store" });
   const message = await res.json();
   return <div>{message.message}</div>;
 }
